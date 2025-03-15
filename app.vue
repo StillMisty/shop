c6e43dd3b4a9a95b9501449e10b87e20af0f48ce
still <1772797842@qq.com>
2025-03-15 11:24:00 +0800

    初始化项目

---
+++ app.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { VueLenis } from "lenis/vue";
+</script>
+
+<template>
+  <VueLenis root>
+    <div>
+      <NuxtLayout>
+        <NuxtPage />
+      </NuxtLayout>
+    </div>
+  </VueLenis>
+</template>
