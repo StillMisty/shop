40c496b48b4dc39cd908fe33ce7bc08a4fb01d1f
still <1772797842@qq.com>
2025-03-18 14:13:25 +0800

    优化订单表组件，添加订单项展示和状态过滤功能，支持横向滚动

--- components/OrderTable.vue
+++ components/OrderTable.vue
@@ -1,6 +1,23 @@
 <template>
   <el-table :data="orders" border>
-    <el-table-column prop="id" label="订单号" width="160"></el-table-column>
+    <el-table-column prop="orderItems" label="订单">
+      <template #default="{ row }">
+        <div
+          class="flex items-center gap-3 overflow-x-auto"
+          @wheel="handleWheel"
+        >
+          <div
+            class="flex flex-col items-center"
+            v-for="item of row.orderItems"
+            :key="item.id"
+          >
+            <el-image :src="item.product.image" class="size-24" fit="cover" />
+            <el-text type="info">{{ item.product.name }}</el-text>
+            <el-text type="info" size="small">{{ item.quantity }}件</el-text>
+          </div>
+        </div>
+      </template>
+    </el-table-column>
     <el-table-column
       prop="orderTotal"
       label="总价"
@@ -12,7 +29,13 @@
       :formatter="(row: any) => new Date(row.orderTime).toLocaleString()"
     >
     </el-table-column>
-    <el-table-column prop="orderStatus" label="状态" width="160">
+    <el-table-column
+      prop="orderStatus"
+      label="状态"
+      width="160"
+      :filters="orderStatusFilters"
+      :filter-method="orderStatusFilterMethod"
+    >
       <template #default="{ row }">
         <div class="flex justify-between items-center">
           <el-tag
@@ -25,7 +48,7 @@
             v-if="row.orderStatus === OrderStatus.PENDING_PAYMENT"
             type="primary"
             size="small"
-            @click="handlePay(row.id)"
+            @click.stop="handlePay(row.id)"
           >
             前去支付
           </el-button>
@@ -41,12 +64,29 @@ import { OrderStatus } from "~/types/OrderType";
 const orderStore = useMyOrderStore();
 const { orders } = storeToRefs(orderStore);
 
+const orderStatusFilters = [
+  { text: "待支付", value: OrderStatus.PENDING_PAYMENT },
+  { text: "已支付", value: OrderStatus.PAID },
+  { text: "处理中", value: OrderStatus.PROCESSING },
+  { text: "已发货", value: OrderStatus.SHIPPED },
+  { text: "已完成", value: OrderStatus.COMPLETED },
+  { text: "已取消", value: OrderStatus.CANCELLED },
+  { text: "退款中", value: OrderStatus.REFUNDING },
+  { text: "已退款", value: OrderStatus.REFUNDED },
+];
+
+const orderStatusFilterMethod = (value: string, row: any) =>
+  row.orderStatus === value;
+
 const handlePay = (id: string) => {
   const router = useRouter();
   router.push(`/orders/${id}`);
-  // orderStore.updateOrder({
-  //   orderId: row.id,
-  //   newStatus: OrderStatus.PAID,
-  // });
+};
+
+const handleWheel = (e: WheelEvent) => {
+  if (e.deltaY !== 0 && e.currentTarget) {
+    e.preventDefault();
+    (e.currentTarget as HTMLElement).scrollLeft += e.deltaY;
+  }
 };
 </script>
